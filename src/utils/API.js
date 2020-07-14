8e0efa7a97d1dfc30d89db1498fe5f7ea730b18d
TGeorge <60828138+TLGeorge@users.noreply.github.com>
2020-07-13 23:41:13 -0400

    api call for random users

---
+++ src/utils/API.js
@@ -0,0 +1,24 @@
+// going to put API axios get function here
+import axios from "axios";
+
+// consider supplying some params to this function to get it to return data in different ways. maybe?
+
+export default {
+    getEmployees: function () {
+        return axios.get(
+            "https://randomuser.me/api/?results=100"
+        ).then(res => {
+            const employees = res.data.results;
+            return employees.map(employee => {
+                return {
+                    image: employee.picture.thumbnail,
+                    firstName: employee.name.first,
+                    lastName: employee.name.last,
+                    phone: employee.phone,
+                    email: employee.email,
+                    dob: employee.dob.date,
+                }
+            })
+        });
+    }
+};
